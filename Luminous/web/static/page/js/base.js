e8b0fc6320907a76d2c693fd65a5fa13e6c09b17
hgyuhyeon <hwgyuhyeon@gmail.com>
2021-06-11 00:39:56 +0900

    add top-down

--- Luminous/web/static/page/js/base.js
+++ Luminous/web/static/page/js/base.js
@@ -1,18 +1,26 @@
-function func(){
-    var name= document.getElementById("name").value;
-    var email= document.getElementById("email").value;
-    var comments = document.getElementById("comments").value;
-    
-    if(name==""||email==""||comments==""){
-      alert("Please fill in all the blanks.");
-      return false;
-    }
-    if(confirm("Name : "+name+"\nEmail : "+email+ "\nComments : "+comments+ "\n\nIs the name and the contents written correct?")==true){
+function func() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var comments = document.getElementById("comments").value;
+
+  if (name == "" || email == "" || comments == "") {
+    alert("Please fill in all the blanks.");
+    return false;
+  }
+  if (confirm("Name : " + name + "\nEmail : " + email + "\nComments : " + comments + "\n\nIs the name and the contents written correct?") == true) {
     alert("submit complete!");
     location.reload();
-    }
-      else{
-        return;
-      }
-    
-    }
+  }
+  else {
+    return;
+  }
+
+}
+
+function goTop() {
+  window.scrollTo(0, 0);
+}
+
+function goBottom() {
+  document.getElementById('bottom').scrollIntoView();
+}
