b0f6b9eb78f1577b3d56f38dcfb750e5d5325b4d
Hongyerim0 <dpfla927@naver.com>
2021-06-10 23:26:19 +0900

    삭제

--- Luminous/web/static/page/js/detail.js
+++ Luminous/web/static/page/js/detail.js
@@ -14,3 +14,24 @@ function calculate(){
 
     alert("Price : $"+price+"\nQuantity : "+Quantity+ "\n"+ "Shipping : $"+shipping+"\nYour final Price : $"+sum+"\n");
   }
+
+  function unlikealert() {
+    alert("The product has been removed in the list of likes.");
+    document.getElementById("Like").style.color = "white";
+    document.getElementById("Like").style.backgroundColor = "#FFA2AD"
+    document.getElementById("border").style.border = "5px solid #FFA2AD";
+  }
+  function likealert() {
+    alert("The product has been reflected in the list of likes.");
+    document.getElementById("Like").style.color = "white";
+    document.getElementById("Like").style.backgroundColor = "#FFA2AD"
+    document.getElementById("border").style.border = "5px solid #FFA2AD";
+  }
+  function cartalert() {
+    alert("The product is in the cart.");
+    document.getElementById("Cart").style.color = "white";
+    document.getElementById("Cart").style.backgroundColor = "#61F3EB";
+  }
+  function buynowalert() {
+    alert("Your order has been completed.");
+  }
--- Luminous/web/static/page/js/shop.js
+++ Luminous/web/static/page/js/shop.js
@@ -32,23 +32,3 @@ function image_onclick2() {
   }
   imgSource.src = arrImage[intRandom];
 }
-function unlikealert() {
-  alert("The product has been removed in the list of likes.");
-  document.getElementById("Like").style.color = "white";
-  document.getElementById("Like").style.backgroundColor = "#FFA2AD"
-  document.getElementById("border").style.border = "5px solid #FFA2AD";
-}
-function likealert() {
-  alert("The product has been reflected in the list of likes.");
-  document.getElementById("Like").style.color = "white";
-  document.getElementById("Like").style.backgroundColor = "#FFA2AD"
-  document.getElementById("border").style.border = "5px solid #FFA2AD";
-}
-function cartalert() {
-  alert("The product is in the cart.");
-  document.getElementById("Cart").style.color = "white";
-  document.getElementById("Cart").style.backgroundColor = "#61F3EB";
-}
-function buynowalert() {
-  alert("Your order has been completed.");
-}
