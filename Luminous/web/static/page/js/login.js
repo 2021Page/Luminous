93d8bb593785f6fd0a2236a237853cc931336d1c
hgyuhyeon <hwgyuhyeon@gmail.com>
2021-06-10 22:04:05 +0900

    add javascript

--- Luminous/web/static/page/js/login.js
+++ Luminous/web/static/page/js/login.js
@@ -1,17 +1,16 @@
-function loginalert(){
-  var id = document.getElementById("x").value;
-  var pw = document.getElementById("y").value;
-  if(id.length==0&&pw.length==0){
+function loginalert() {
+  var id = document.getElementById("username").value;
+  var pw = document.getElementById("password").value;
+  if (id.length == 0 && pw.length == 0) {
     alert("ID, Password are required!!");
   }
-  else if(id.length==0&&pw.length!=0){
+  else if (id.length == 0 && pw.length != 0) {
     alert("ID is required!!");
   }
-  else if(id.length!=0&&pw.length==0){
+  else if (id.length != 0 && pw.length == 0) {
     alert("Password is required!!");
   }
-  else{
-    alert("Welcome to Luminous <"+id+">");
-    document.location.href="main.html";
-}
+  else {
+    alert("Welcome to Luminous <" + id + ">");
+  }
 }
