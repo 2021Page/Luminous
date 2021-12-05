ea52a39916b1d6429926dd9cbb60a21783ffa608
Hongyerim0 <dpfla927@naver.com>
2021-12-05 21:42:31 +0900

    디테일 페이지에서 상품 구매시 DB처리

--- web/views2.py
+++ web/views2.py
@@ -80,12 +80,23 @@ def detail_buy(request, product_id):
     curs.execute(price)
     data_price = curs.fetchall()
 
+    #현재시간
     time="SELECT DATE_FORMAT(CURDATE(), '%Y-%m-%d')"
     curs.execute(time)
     data_time = curs.fetchall()
-    sql= "INSERT into order_info(order_Date, total_Price, order_Status, user_ID, order_product) values(%s,%s,%s,%s,%s)"
-    curs.execute(sql,(data_time[0][0], data_price[0][0], 'completed', userID, data_title[0][0]))
+
+    #order_info  테이블에 삽입
+    sql= "INSERT into order_info(order_Date, total_Price, order_Status, user_ID) values(%s,%s,%s,%s)"
+    curs.execute(sql,(data_time[0][0], data_price[0][0], 'completed', userID))
+
+    #order_id 불러오기
+    sql = "SELECT order_ID from order_info ORDER BY order_ID DESC LIMIT 1"
+    curs.execute(sql)
+    data_order = curs.fetchall()
+
+    #buy 테이블에 삽입
+    sql= "INSERT into buy(order_ID, Product_ID, order_p_name) values(%s,%s,%s)"
+    curs.execute(sql,(data_order[0][0], product_id, data_title[0][0]))
     con.commit()
-    data = curs.fetchall()
     con.close()
     return redirect('detail', product_id)
